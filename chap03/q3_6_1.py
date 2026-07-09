c176328ccda357a5e9aa16d4926fa25089e29e96
itc-s26005 <s26005@std.it-college.ac.jp>
2026-07-09 02:47:25 +0000

    3.6 whileを使ったプログラミング

--- chap03/q3_6_1.py
+++ chap03/q3_6_1.py
@@ -1,5 +1,5 @@
 import random
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+numbers = [chr(i) for i in range(ord('0'), ord('9') + 1)]
 sample4 = random.sample(numbers, k=4)
 num4 = ''.join(sample4)
 while True:
