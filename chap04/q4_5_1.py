c1b9900c834387622dfbe55bb206e01779bcb664
itc-s26005 <s26005@std.it-college.ac.jp>
2026-07-16 02:31:03 +0000

    4.5 関数はオブジェク

---
+++ chap04/q4_5_1.py
@@ -0,0 +1,8 @@
+numbers = range(1, 11)
+list(numbers)
+
+functions = [sum, min, max]
+number_list = range(1, 11)
+for func in functions:
+    print("Function: {},Result: {}".format(
+        func.__name__, func(number_list)))
