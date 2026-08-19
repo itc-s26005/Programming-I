fd72355f9c434a822cef3bcf3bc5c0bd6e07cfd9
itc-s26005 <s26005@std.it-college.ac.jp>
2026-08-19 01:27:44 +0000

    7.1 エラーと例外の基本

---
+++ chap07/q7_1_1.py
@@ -0,0 +1,9 @@
+name_age = {'tanaka': 35, 'satou': 25, 'suzuki': 27}
+
+def dict_info(dict_tbl, key):
+    try:
+        return dict_tbl[key]
+    except:
+        return 'key is not fornd'
+print(dict_info(name_age, 'satou'))
+print(dict_info(name_age, 'yamada'))
